0bc07023e9398881667683cd43d794dc5388068f
vortexisalpha <joshuaboshua250505@gmail.com>
2026-01-18 18:59:08 -0500

    updated bash for comparing inputs, outputs and expected on error

--- 1-regexes/test_lexer.sh
+++ 1-regexes/test_lexer.sh
@@ -2,20 +2,20 @@
 
 # Try to find a portable way of getting rid of
 # any stray carriage returns
-if which dos2unix ; then
-    DOS2UNIX="dos2unix"
-elif which fromdos ; then
-    DOS2UNIX="fromdos"
+if which dos2unix; then
+  DOS2UNIX="dos2unix"
+elif which fromdos; then
+  DOS2UNIX="fromdos"
 else
-    >&2 echo "warning: dos2unix is not installed."
-    # This should work on Linux and MacOS, it matches all the carriage returns with sed and removes
-    # them.  `tr` is used instead of `sed` because some versions of `sed` don't recognize the
-    # carriage return symbol.  Something similar could be implemented with `sed` if necessary or
-    # worst case it could be disabled by substituting it with `cat`.  One other thing to note is
-    # that there are no quotes around the \r, which is so that it gets converted by bash.
-    DOS2UNIX="tr -d \r"
-    # DOS2UNIX="sed -e s/\r//g"
-    # DOS2UNIX="cat"
+  >&2 echo "warning: dos2unix is not installed."
+  # This should work on Linux and MacOS, it matches all the carriage returns with sed and removes
+  # them.  `tr` is used instead of `sed` because some versions of `sed` don't recognize the
+  # carriage return symbol.  Something similar could be implemented with `sed` if necessary or
+  # worst case it could be disabled by substituting it with `cat`.  One other thing to note is
+  # that there are no quotes around the \r, which is so that it gets converted by bash.
+  DOS2UNIX="tr -d \r"
+  # DOS2UNIX="sed -e s/\r//g"
+  # DOS2UNIX="cat"
 fi
 
 echo "========================================"
@@ -24,7 +24,7 @@ make clean
 echo " Force building"
 make nocomment -B
 if [[ "$?" -ne 0 ]]; then
-    echo "Build failed.";
+  echo "Build failed."
 fi
 echo ""
 mkdir -p test/out
@@ -35,47 +35,52 @@ PASSED=0
 CHECKED=0
 
 for i in test/in/*.txt; do
-    echo "==========================="
-    echo ""
-    echo "Input file : ${i}"
-    BASENAME=$(basename $i .txt);
-    cat $i | ./nocomment  > test/out/$BASENAME.stdout.txt  2> test/out/$BASENAME.stderr.txt
+  echo "==========================="
+  echo ""
+  echo "Input file : ${i}"
+  BASENAME=$(basename $i .txt)
+  cat $i | ./nocomment >test/out/$BASENAME.stdout.txt 2>test/out/$BASENAME.stderr.txt
 
-    diff <(cat test/ref/$BASENAME.stdout.txt) <(cat test/out/$BASENAME.stdout.txt) > test/out/$BASENAME.diff.txt
-    if [[ "$?" -ne "0" ]]; then
-        echo -e "\nERROR"
-    else
-        PASSED=$(( ${PASSED}+1 ));
-    fi
-    CHECKED=$(( ${CHECKED}+1 ));
+  diff <(cat test/ref/$BASENAME.stdout.txt) <(cat test/out/$BASENAME.stdout.txt) >test/out/$BASENAME.diff.txt
+  if [[ "$?" -ne "0" ]]; then
+    echo -e "\nERROR"
+    echo "----- Input -------"
+    cat test/in/$BASENAME.txt
+    echo "----- Your output -----"
+    cat test/out/$BASENAME.stdout.txt
+    echo "----- Expected output -----"
+    cat test/ref/$BASENAME.stdout.txt
+  else
+    PASSED=$((${PASSED} + 1))
+  fi
+  CHECKED=$((${CHECKED} + 1))
 done
-
 
 echo "########################################"
 echo "Passed ${PASSED} out of ${CHECKED}".
 echo ""
 
 case "$(uname -s)" in
-    Darwin)
-        echo ""
-        echo "Warning: This appears not to be a Linux environment"
-        echo "         Make sure you do a final run on a lab machine or an Ubuntu VM"
-        exit 0
-        ;;
+Darwin)
+  echo ""
+  echo "Warning: This appears not to be a Linux environment"
+  echo "         Make sure you do a final run on a lab machine or an Ubuntu VM"
+  exit 0
+  ;;
 esac
 
 RELEASE=$(lsb_release -d)
 if [[ $? -ne 0 ]]; then
-    echo ""
-    echo "Warning: This appears not to be a Linux environment"
-    echo "         Make sure you do a final run on a lab machine or an Ubuntu VM"
+  echo ""
+  echo "Warning: This appears not to be a Linux environment"
+  echo "         Make sure you do a final run on a lab machine or an Ubuntu VM"
 else
-    grep -q "Ubuntu 22.04" <(echo $RELEASE)
-    FOUND=$?
+  grep -q "Ubuntu 22.04" <(echo $RELEASE)
+  FOUND=$?
 
-    if [[ $FOUND -ne 0 ]]; then
-        echo ""
-        echo "Warning: This appears not to be the target environment"
-        echo "         Make sure you do a final run on a lab machine or an Ubuntu VM"
-    fi
+  if [[ $FOUND -ne 0 ]]; then
+    echo ""
+    echo "Warning: This appears not to be the target environment"
+    echo "         Make sure you do a final run on a lab machine or an Ubuntu VM"
+  fi
 fi
